d0661c3a2a4bdd8ec68c66aee80fe6aa4b59e0d0
laggu <kwla103@naver.com>
2017-07-24 21:45:28 +0900

    First commit(안드로이드)
    프로젝트 생성
    FCM 메시지 구현 (테스트X)

---
+++ Android/app/src/main/java/beyond_imagination/blubblub/pService/FCMMessagingService.java
@@ -0,0 +1,50 @@
+package beyond_imagination.blubblub.pService;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import beyond_imagination.blubblub.MainActivity;
+import beyond_imagination.blubblub.R;
+
+/**
+ * Created by laggu on 2017-07-24.
+ */
+
+public class FCMMessagingService extends com.google.firebase.messaging.FirebaseMessagingService{
+    private static final String TAG = "FirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //super.onMessageReceived(remoteMessage);
+        sendNotification(remoteMessage.getData().get("message"));
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("FCM Push Test")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+}
