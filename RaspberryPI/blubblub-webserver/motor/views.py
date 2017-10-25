fb5f96672ee8113d5d9011075c28407abbdff817
laggu <kwla103@naver.com>
2017-10-25 06:25:10 +0000

    add models for lastfeeding time

--- RaspberryPI/blubblub-webserver/motor/views.py
+++ RaspberryPI/blubblub-webserver/motor/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .motor import run
+from motor.models import FeedingTime
 from authenticate.models import TokenRegister
-import sys, time
-
+import sys, time, os
+from sensor.data import Data
 
 # Create your views here.
 
@@ -11,13 +12,19 @@ import sys, time
 def feeding(request):
     token = request.POST['token']
     objects = TokenRegister.objects.all()
-    tokenList = [ t.token for t in objects]
+    tokenList = [t.token for t in objects]
     print(tokenList)
     result = 0
     if token in tokenList:
+        t = time.localtime()
+        Data.time = t.tm_hour*60 + t.tm_min
+
+        feedingtime = FeedingTime.objects.get(pk=1)
+        feedingtime.lasttime = Data.time
+        feedingtime.save()
+
         run()
         result = 1
-    t = time.localtime()
-    Data.time = t.tm_hour*60 + t.tm_min
+    print(Data.time, t.tm_hour*60 + t.tm_min)
     context={'time':result}
     return render(request, 'motor/feeding.html', context)
--- RaspberryPI/blubblub-webserver/sensor/data.py
+++ RaspberryPI/blubblub-webserver/sensor/data.py
@@ -1,5 +1,6 @@
+from motor.models import FeedingTime
 class Data():
 	temperature=0
 	illuminance = 0
 	turbidity = 0
-	time = 0
+	time = FeedingTime.objects.get(pk=1).lasttime
